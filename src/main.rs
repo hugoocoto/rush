cd0198eb121a8f2184939d4474ac5d29c95e48c3
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-02-08 14:26:03 +0100

    Adding parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::{Write, stdin, stdout};
+
+const PROMPT: &str = ">> ";
+
+fn run(input: &str) {
+    let command = input.trim().split(" ").filter(|x| x.len() > 0);
+    for p in command {
+        print!("({p})");
+    }
+    println!("");
+}
+
 pub fn main() {
     println!("Hello, World!");
+
+    loop {
+        let mut input = String::new();
+        print!("{PROMPT}");
+        let _ = stdout().flush();
+        let _ = stdin().read_line(&mut input);
+        run(&input);
+    }
 }
