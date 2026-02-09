5c6e903a60d4f57f9f55cf9deab1812f387dd5e5
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-02-09 10:44:32 +0100

    improving parsing

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,89 @@ use std::{
     process::Command,
 };
 
-type Builtin = dyn Fn(Vec<&str>) -> ();
+type Builtin = dyn Fn(Vec<String>) -> ();
 const PROMPT: &str = ">> ";
 
-fn exec(input: Vec<&str>) {
-    if input.len() == 0 {
-        return;
+fn exec(input: Vec<String>) {
+    if let Some(e) = input.get(0) {
+        let arguments = &input[1..];
+        for a in arguments {
+            print!("({a})");
+        }
+        println!("");
+        let command = Command::new(e)
+            .args(arguments)
+            .output()
+            .expect("Can't create command");
+        stdout()
+            .write_all(&command.stdout)
+            .expect("Can't write to stdout");
+        stderr()
+            .write_all(&command.stderr)
+            .expect("Can't write to stderr");
     }
-    let command = Command::new("sh")
-        .arg("-c")
-        .arg(input.join(" "))
-        .output()
-        .expect("Can't create command");
-    let _ = stdout().write_all(&command.stdout);
-    let _ = stderr().write_all(&command.stderr);
 }
 
-fn run(input: &str, builtins: &HashMap<&str, Box<Builtin>>) {
-    let mut command: Vec<&str> = Vec::new();
-    for elem in input.trim().split(" ") {
+fn parse(input: String) -> Vec<String> {
+    let mut arr = Vec::<String>::new();
+    let mut start = 0;
+    let mut esq = false;
+    let mut on_str = false;
+    for i in 0..input.len() {
+        if let Some(c) = input.chars().nth(i) {
+            if esq {
+                esq = false;
+            } else if on_str {
+                if c == '"' {
+                    on_str = false;
+                }
+            } else if c == '"' {
+                on_str = true;
+            } else if c == '\\' {
+                esq = true;
+            } else if c.is_whitespace() {
+                arr.push(String::from(input.get(start..i).unwrap()));
+                start = i + 1;
+            }
+        } else {
+            break;
+        }
+    }
+    if start < input.len() {
+        arr.push(String::from(input.get(start..).unwrap()));
+    }
+
+    return arr;
+}
+
+fn run(input: String, builtins: &HashMap<String, Box<Builtin>>) {
+    let mut command: Vec<String> = Vec::new();
+    for elem in parse(String::from(input.trim())) {
         if elem.len() > 0 {
             command.push(elem);
         }
     }
-    if builtins.contains_key(command[0]) {
-        builtins.get(command[0]).unwrap()(command);
+    if builtins.contains_key(&command[0]) {
+        builtins.get(&command[0]).unwrap()(command);
     } else {
         exec(command);
     }
 }
 
-fn hello(_command: Vec<&str>) {
+fn hello(_command: Vec<String>) {
+    assert!(_command[0] == "hello");
     println!("Hello, World!");
 }
 
 pub fn main() {
-    let mut builtin_table: HashMap<&str, Box<Builtin>> = HashMap::new();
-    builtin_table.insert("hello", Box::new(hello));
+    let mut builtin_table: HashMap<String, Box<Builtin>> = HashMap::new();
+    builtin_table.insert(String::from("hello"), Box::new(hello));
 
     loop {
         let mut input = String::new();
         print!("{PROMPT}");
-        let _ = stdout().flush();
-        let _ = stdin().read_line(&mut input);
-        run(&input, &builtin_table);
+        stdout().flush().unwrap();
+        stdin().read_line(&mut input).unwrap();
+        run(input, &builtin_table);
     }
 }
